7bc476e970a265e0d5e4bb13e584d456a1fec1bd
jiaruizhou <jiaruizhou@mail.ustc.edu.cn>
2024-11-21 19:57:32 +0800

    add the code for contrastive training

---
+++ contrastive_finetune.py
@@ -0,0 +1,192 @@
+import torch
+from typing import Iterable, Optional
+import util.misc as misc
+import util.lr_sched as lr_sched
+import math
+import torch.distributed as dist
+
+
+def unwrap_ddp(model):
+    if hasattr(model, "module"):
+        return unwrap_ddp(model.module)
+    else:
+        return model
+
+
+def to_device(x, device, non_blocking=True):
+    if isinstance(x, torch.Tensor):
+        x = x.to(device, non_blocking=non_blocking)
+    elif isinstance(x, list) or isinstance(x, tuple):
+        for i in range(len(x)):
+            x[i] = to_device(x[i], device, non_blocking=non_blocking)
+    elif isinstance(x, dict):
+        for k in x:
+            x[k] = to_device(x[k], device, non_blocking=non_blocking)
+    else:
+        raise ValueError(f"Unsupported type: {type(x)}")
+    return x
+
+
+class AllGather(torch.autograd.Function):
+    """ 
+    all_gather with gradient back-propagation
+    """
+    @staticmethod
+    def forward(ctx, tensor_list, tensor):
+        dist.all_gather(tensor_list, tensor)
+        return tuple(tensor_list)
+
+    @staticmethod
+    def backward(ctx, *grad_list):
+        grad_list = list(grad_list)
+        rank = dist.get_rank()
+
+        dist_ops = [
+            dist.reduce(grad_list[i], i, async_op=True) for i in range(dist.get_world_size())
+        ]
+
+        for op in dist_ops:
+            op.wait()
+
+        return None, grad_list[rank] 
+
+
+
+def train_one_epoch(model: torch.nn.Module,
+                    criterion: torch.nn.Module,
+                    data_loader: Iterable,
+                    optimizer: torch.optim.Optimizer,
+                    device: torch.device,
+                    epoch: int,
+                    loss_scaler,
+                    max_norm: float = 0,
+                    log_writer=None,
+                    args=None):
+    model.train(True)
+    if not args.train_bert:
+        unwrap_ddp(model).textmodel.eval()
+    if not args.train_vit:
+        unwrap_ddp(model).visualmodel.eval()
+    metric_logger = misc.MetricLogger(delimiter="  ")
+    metric_logger.add_meter('lr', misc.SmoothedValue(window_size=1, fmt='{value:.6f}'))
+    header = 'Epoch: [{}]'.format(epoch)
+    print_freq = 100
+    accum_iter = args.accum_iter
+    optimizer.zero_grad()
+
+    if log_writer is not None:
+        print('log_dir: {}'.format(log_writer.log_dir))
+
+    for data_iter_step, (samples, _) in enumerate(metric_logger.log_every(data_loader, print_freq, header)):
+
+        # we use a per iteration (instead of per epoch) lr scheduler
+        if data_iter_step % accum_iter == 0:
+            lr_sched.adjust_learning_rate(optimizer, data_iter_step / len(data_loader) + epoch, args)
+        
+            #  (fcgr,(token_id,token_ids))
+        samples = to_device(samples, device, non_blocking=True)
+        
+
+        with torch.amp.autocast('cuda', enabled=args.amp):
+            visual_feature, text_feature = model(samples)
+
+        if args.cross_process_negatives:
+            visual_feature = visual_feature.contiguous()
+            text_feature = text_feature.contiguous()
+            bs = args.batch_size
+            if not (torch.distributed.is_available() and torch.distributed.is_initialized()):
+                raise ValueError("cross_process_negatives requires torch.distributed to be initialized")
+            visual_features = [torch.zeros_like(visual_feature) for _ in range(torch.distributed.get_world_size())]
+            AllGather.apply(visual_features, visual_feature)
+            visual_feature = torch.cat(visual_features, dim=0)
+            text_features = [torch.zeros_like(text_feature) for _ in range(torch.distributed.get_world_size())]
+            AllGather.apply(text_features, text_feature)
+            text_feature = torch.cat(text_features, dim=0)
+
+        loss = criterion(visual_feature,text_feature)
+        visual_feature, text_feature = visual_feature.detach().cpu(), text_feature.detach().cpu()
+        visual_feature = visual_feature / visual_feature.norm(p=2, dim=-1, keepdim=True)
+        text_feature = text_feature / text_feature.norm(p=2, dim=-1, keepdim=True)
+        sim_matrix = torch.mm(visual_feature, text_feature.t())
+        
+        sorted_indices = torch.argsort(sim_matrix, dim=1, descending=True)
+        ranks = torch.zeros_like(sim_matrix, dtype=torch.long)
+        rank = ranks.scatter_(1, sorted_indices, torch.arange(sim_matrix.size(1)).expand_as(sim_matrix)).diag() + 1
+        assert rank.dim() == 1
+        avg_rank = rank.float().mean().item()
+
+        loss_value = loss.item()
+        loss_value_reduce = misc.all_reduce_mean(loss_value)
+        if not math.isfinite(loss_value_reduce):
+            print("[Warning] Loss is {}, skipped.".format(loss_value))
+            continue
+        if not args.single_gpu:
+            torch.distributed.barrier()
+
+        loss /= accum_iter
+        # with torch.autograd.detect_anomaly():
+        loss_scaler(loss, optimizer, clip_grad=max_norm, parameters=model.parameters(), create_graph=False, update_grad=(data_iter_step + 1) % accum_iter == 0)
+
+        if (data_iter_step + 1) % accum_iter == 0:
+            optimizer.zero_grad()
+
+        torch.cuda.synchronize()
+
+        metric_logger.update(loss=loss_value)
+        min_lr = 10.
+        max_lr = 0.
+        for group in optimizer.param_groups:
+            min_lr = min(min_lr, group["lr"])
+            max_lr = max(max_lr, group["lr"])
+
+        metric_logger.update(lr=max_lr)
+
+        
+        if log_writer is not None and (data_iter_step + 1) % accum_iter == 0:
+            global_step = epoch * len(data_loader) // accum_iter + data_iter_step // accum_iter
+            log_writer.add_scalar('train/loss', loss_value_reduce, global_step)
+            log_writer.add_scalar('train/lr', max_lr, global_step)
+            log_writer.add_scalar('train/rank', avg_rank, global_step)
+
+    # gather the stats from all processes
+    metric_logger.synchronize_between_processes()
+    print("Averaged stats:", metric_logger)
+    return {k: meter.global_avg for k, meter in metric_logger.meters.items()}
+
+@torch.no_grad()
+def evaluate(args, data_loader, model, criterion, device):
+
+    metric_logger = misc.MetricLogger(delimiter="  ")
+    header = 'Test:'
+    # switch to evaluation mode
+    model.eval()
+    for batch in metric_logger.log_every(data_loader, 40, header):
+        images = batch[0]
+        
+        to_device(images, device)
+        
+        # compute output
+        with torch.amp.autocast('cuda', enabled=args.amp):
+            visual_feature, text_feature = model(images)
+            loss = criterion(visual_feature,text_feature)
+        
+        visual_feature, text_feature = visual_feature.detach().cpu(), text_feature.detach().cpu()
+
+        visual_feature = visual_feature / visual_feature.norm(p=2, dim=-1, keepdim=True)
+        visual_feature = text_feature / text_feature.norm(p=2, dim=-1, keepdim=True)
+        
+        sim_matrix = visual_feature @ text_feature.t()
+
+        sorted_indices = torch.argsort(sim_matrix, dim=1, descending=True)
+        ranks = torch.zeros_like(sim_matrix, dtype=torch.long)
+        rank = ranks.scatter_(1, sorted_indices, torch.arange(sim_matrix.size(1)).expand_as(sim_matrix)).diag() + 1
+        assert rank.dim() == 1
+        avg_rank = rank.float().mean().item()
+        
+        metric_logger.meters['loss'].update(loss.item())
+        metric_logger.meters['avg_rank'].update(avg_rank)
+        
+
+    metric_logger.synchronize_between_processes()
+    
+    return {k: meter.global_avg for k, meter in metric_logger.meters.items()}
---
+++ contrastive_main.py
@@ -0,0 +1,326 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+# --------------------------------------------------------
+# References:
+# DeiT: https://github.com/facebookresearch/deit
+# BEiT: https://github.com/microsoft/unilm/tree/master/beit
+# --------------------------------------------------------
+
+import argparse
+import datetime
+import json
+import timm.optim.optim_factory as optim_factory
+import os
+# os.environ["CUDA_LAUNCH_BLOCKING"] = "1"
+import time
+import random
+from pathlib import Path
+
+import torch
+import torch.backends
+import torch.backends.cudnn as cudnn
+from torch.utils.tensorboard import SummaryWriter
+import numpy as np
+
+# assert timm.__version__ == "0.3.2"  # version check
+
+import util.lr_decay as lrd
+import util.misc as misc
+
+
+from util.misc import NativeScalerWithGradNormCount as NativeScaler
+
+from models.model_contrastive import contrastive_dna_model
+
+from contrastive_finetune import train_one_epoch, evaluate
+
+from util.custom_datasets import Visual_Text_Dataset
+from torch.utils.data import random_split
+
+import os
+
+from util.contrastive_utils import ContrastiveLoss, InfoNCE, CLIPLoss
+
+# torch.autograd.set_detect_anomaly(True)
+
+def get_args_parser():
+    parser = argparse.ArgumentParser('MAE fine-tuning for image classification', add_help=False)
+    
+    # Optimizer parameters
+    parser.add_argument('--clip_grad', type=float, default=None, metavar='NORM', help='Clip gradient norm (default: None, no clipping)')
+    parser.add_argument('--weight_decay', type=float, default=0.05, help='weight decay (default: 0.05)')
+    parser.add_argument('--lr', type=float, default=None, metavar='LR', help='learning rate (absolute lr)')
+    parser.add_argument('--blr', type=float, default=1e-3, metavar='LR', help='base learning rate: absolute_lr = base_lr * total_batch_size / 256')
+    parser.add_argument('--layer_decay', type=float, default=0.75, help='layer-wise lr decay from ELECTRA/BEiT')
+    parser.add_argument('--min_lr', type=float, default=1e-6, metavar='LR', help='lower lr bound for cyclic schedulers that hit 0')
+    parser.add_argument('--warmup_epochs', type=int, default=2, metavar='N', help='epochs to warmup LR')
+
+    # Augmentation parameters
+    parser.add_argument('--color_jitter', type=float, default=None, metavar='PCT', help='Color jitter factor (enabled only when not using Auto/RandAug)')
+    parser.add_argument('--aa', type=str, default='rand-m9-mstd0.5-inc1', metavar='NAME', help='Use AutoAugment policy. "v0" or "original". " + "(default: rand-m9-mstd0.5-inc1)'),
+
+    # * Random Erase params
+    parser.add_argument('--reprob', type=float, default=0.25, metavar='PCT', help='Random erase prob (default: 0.25)')
+    parser.add_argument('--remode', type=str, default='pixel', help='Random erase mode (default: "pixel")')
+    parser.add_argument('--recount', type=int, default=1, help='Random erase count (default: 1)')
+    parser.add_argument('--resplit', action='store_true', default=False, help='Do not random erase first (clean) augmentation split')
+
+    # * Finetuning params
+    parser.add_argument('--finetune', default='', help='finetune from checkpoint')
+    # parser.add_argument('--global_pool', action='store_true')
+    # parser.set_defaults(global_pool=False)
+    
+    
+    parser.add_argument('--cls_token', action='store_false', dest='global_pool', help='Use class token instead of global pool for classification')
+    
+    # training parameters
+    parser.add_argument('--batch_size', default=64, type=int, help='Batch size per GPU (effective batch size is batch_size * accum_iter * # gpus')
+    parser.add_argument('--epochs', default=50, type=int)
+    parser.add_argument('--accum_iter', default=1, type=int, help='Accumulate gradient iterations (for increasing the effective batch size under memory constraints)')
+
+    parser.add_argument('--start_epoch', default=0, type=int, metavar='N', help='start epoch')
+    parser.add_argument('--eval', action='store_true', help='Perform evaluation only')
+    parser.add_argument('--dist_eval', action='store_true', default=False, help='Enabling distributed evaluation (recommended during training for faster monitor')
+    parser.add_argument('--num_workers', default=0, type=int)
+    parser.add_argument('--pin_mem', action='store_true', help='Pin CPU memory in DataLoader for more efficient (sometimes) transfer to GPU.')
+    parser.add_argument('--no_pin_mem', action='store_false', dest='pin_mem')
+    parser.add_argument('--pred', action='store_true', help='Perform evaluation only')
+
+    parser.set_defaults(pin_mem=False)
+
+    # distributed training parameters
+    parser.add_argument('--world_size', default=1, type=int, help='number of distributed processes')
+    parser.add_argument('--local-rank', default=-1, type=int)
+    parser.add_argument('--dist_on_itp', action='store_true')
+    parser.add_argument('--dist_url', default='env://', help='url used to set up distributed training')
+
+    # DNA data set
+    parser.add_argument('--data', default="", type=str, help="the dataset name for tax")
+    parser.add_argument('--kmer', default=5, type=int, help="the k-mer size of fcgr")  # input size 2**k x 2**k
+
+    # Dataset parameters
+    parser.add_argument('--data_path', default='./data/', type=str, help='dataset path')
+    parser.add_argument('--output_dir', default='./contrastive_output/', help='path where to save, empty for no saving')
+    parser.add_argument('--log_dir', default='./contrastive_output/', help='path where to tensorboard log')
+    parser.add_argument('--device', default='cuda', help='device to use for training / testing')
+    parser.add_argument('--seed', default=0, type=int)
+
+    # vit Model set
+    parser.add_argument('--vit_model', default='vit_base_patch4_5mer', type=str, metavar='MODEL', help='Name of model to train')
+    parser.add_argument('--input_size', default=224, type=int, help='images input size')
+    parser.add_argument('--cls_num', default=5, help="the class number used for vit head")  #['superkingdom', 'kingdom', 'phylum', 'family']
+    parser.add_argument('--vit_resume', default='./output/output_b_p4_5mer/checkpoint-540.pth', help='resume from vit checkpoint')
+    parser.add_argument('--vit_embed_dim', default=768, type=int, help='Dimension of the vision transformer')
+
+    # BERT Model paramerters
+    parser.add_argument('--seq_len', help=' ', type=int, default=502)
+    parser.add_argument('--bert_resume', default='./bertax_pytorch', help='resume from bert checkpoint')
+    parser.add_argument('--config_path', default='./bertax_pytorch/config.json', help='resume from bert checkpoint')
+    parser.add_argument('--bert_embed_dim', default=250, type=int, help='Dimension of the vision transformer')
+
+
+    # pooling
+    # parser.add_argument('--classfication_global_pool', action='store_true')
+    # parser.set_defaults(classfication_global_pool=True)
+    parser.add_argument('--global_pooling_vit_bert', action='store_true', help='Use global pooled features of ViT and BERT')
+    parser.add_argument('--global_pool_vit', action='store_true', help='Use global pooled features of ViT')
+    # for vl model
+    parser.add_argument('--model', default='fuse', type=str, metavar='MODEL', help='Name of model to train')
+    parser.add_argument('--drop_path', type=float, default=0.1, metavar='PCT', help='Drop path rate (default: 0.1)')
+    parser.add_argument('--resume', default='', help='resume from fuse model checkpoint')
+
+    parser.add_argument('--train_vit', action='store_true', help='train the vit')
+    parser.add_argument('--train_bert', action='store_true', help='train the vit')
+    parser.add_argument('--pooling_method', default='cls_output', choices=["cls_output", "pooler_output", "average_pooling"], help='the bert feature')
+    parser.add_argument('--visual_token_nums', default=65, type=int)
+    parser.add_argument('--text_token_nums', default=502, type=int)
+    parser.add_argument('--vl_hidden_dim', default=256, type=int, help='Size of the embeddings (dimension of the vision-language transformer)')
+    parser.add_argument('--vl_dropout', default=0.1, type=int, help="Dropout applied in the vision-language transformer")
+    parser.add_argument('--vl_nheads', default=8, type=int, help="Number of attention heads inside the vision-language transformer's attentions")
+    parser.add_argument('--vl_dim_feedforward', default=1024, type=int, help="Intermediate size of the feedforward layers in the vision-language transformer blocks")
+    parser.add_argument('--vl_enc_layers', default=1, type=int, help='Number of encoders in the vision-language transformer')
+    parser.add_argument('--single_gpu', action='store_true', help='Use single GPU to train')
+    parser.add_argument('--all_head_trunc', action='store_true', help='Use trunc norm for all mlp head')
+
+    parser.add_argument('--amp', action='store_true', help='Enable Automatic Mixed Precision')
+    parser.add_argument('--loss_scale', action='store_true', help='Enable loss scaling')
+    parser.add_argument('--vit2bert_proj',action='store_true', help='do not use layernorm all mlp head')
+    parser.add_argument('--no_norm_before_head',action='store_true', help='do not use layernorm all mlp head')
+
+    parser.add_argument('--loss_fn',default="clip_loss", choices=["contrastive","infonce","clip_loss"], help="The contrastive loss.")
+    parser.add_argument('--cross_process_negatives',action='store_true', help="Gather all negatives from other gpus")
+    parser.add_argument('--use_logit_scale',action='store_true', help="use learnerable temperature for clip loss")
+    return parser
+
+
+
+def main(args):
+    if not args.pred:
+        args.data_path = os.path.join(args.data_path, args.data + "/")
+
+    args.output_dir = os.path.join(args.output_dir, args.model,args.data)
+    
+    if args.output_dir:
+        Path(args.output_dir).mkdir(parents=True, exist_ok=True)
+    
+    args.log_dir = args.output_dir
+    misc.init_distributed_mode(args)
+
+    print('job dir: {}'.format(os.path.dirname(os.path.realpath(__file__))))
+    print("{}".format(args).replace(', ', ',\n'))
+
+    device = torch.device(args.device)
+
+    # fix the seed for reproducibility
+    # We do not add RANK to seed for correct data spliting in distributed training.
+    seed = args.seed
+    random.seed(seed)
+    torch.manual_seed(seed)
+    np.random.seed(seed)
+
+    if not args.eval:
+        dataset = Visual_Text_Dataset(args, files=args.data_path, kmer=args.kmer, phase="train")
+
+        train_size = int(0.95 * len(dataset))
+        val_size = len(dataset) - train_size
+
+        # 使用 random_split 进行分割
+        dataset_train, _ = random_split(dataset, [train_size, val_size])
+        # todo change the datasets
+        dataset_val = Visual_Text_Dataset(args, files=args.data_path, kmer=args.kmer, phase="test")
+
+    if args.eval:
+        dataset_val = Visual_Text_Dataset(args, files=args.data_path, kmer=args.kmer, phase="test")
+    
+    fuse_model = contrastive_dna_model(args)
+    fuse_model.to(device)
+
+    # fix the seed for reproducibility
+    seed = args.seed + misc.get_rank()
+    random.seed(seed)
+    torch.manual_seed(seed)
+    np.random.seed(seed)
+
+    if True:  # args.distributed:
+        num_tasks = misc.get_world_size()
+        global_rank = misc.get_rank()
+        if not args.eval:
+            print("dataset {} for {} is ready".format(args.data_path, args.kmer))
+            sampler_train = torch.utils.data.DistributedSampler(dataset_train, num_replicas=num_tasks, rank=global_rank, shuffle=True)
+            print("Sampler_train = %s" % str(sampler_train))
+        if args.dist_eval:
+            if len(dataset_val) % num_tasks != 0:
+                print('Warning: Enabling distributed evaluation with an eval dataset not divisible by process number. '
+                      'This will slightly alter validation results as extra duplicate entries are added to achieve '
+                      'equal num of samples per-process.')
+            sampler_val = torch.utils.data.DistributedSampler(dataset_val, num_replicas=num_tasks, rank=global_rank, shuffle=True)  # shuffle=True to reduce monitor bias
+        else:
+            sampler_val = torch.utils.data.SequentialSampler(dataset_val)
+    else:
+        sampler_train = torch.utils.data.RandomSampler(dataset_train)
+        sampler_val = torch.utils.data.SequentialSampler(dataset_val)
+
+    if global_rank == 0 and args.log_dir is not None and not args.eval:
+        os.makedirs(args.log_dir, exist_ok=True)
+        log_writer = SummaryWriter(log_dir=args.log_dir)
+    else:
+        log_writer = None
+    if not args.eval:
+        data_loader_train = torch.utils.data.DataLoader(
+            dataset_train,
+            sampler=sampler_train,
+            batch_size=args.batch_size,
+            num_workers=args.num_workers,
+            pin_memory=args.pin_mem,
+            drop_last=True,
+        )
+        
+    data_loader_val = torch.utils.data.DataLoader(dataset_val, sampler=sampler_val, batch_size=args.batch_size, num_workers=args.num_workers, pin_memory=args.pin_mem, drop_last=False)
+
+    model_without_ddp = fuse_model
+    n_parameters = sum(p.numel() for p in fuse_model.parameters() if p.requires_grad)
+
+    # print("Model = %s" % str(model_without_ddp))
+    print('number of params (M): %.2f' % (n_parameters / 1.e6))
+
+    eff_batch_size = args.batch_size * args.accum_iter * misc.get_world_size()
+
+    if args.lr is None:  # only base_lr is specified
+        args.lr = args.blr * eff_batch_size / 256
+
+    print("base lr: %.2e" % (args.lr * 256 / eff_batch_size))
+    print("actual lr: %.2e" % args.lr)
+
+    print("accumulate grad iterations: %d" % args.accum_iter)
+    print("effective batch size: %d" % eff_batch_size)
+
+    if args.distributed:
+        fuse_model = torch.nn.parallel.DistributedDataParallel(fuse_model, device_ids=[args.gpu])
+        model_without_ddp = fuse_model.module
+
+
+
+    param_groups = lrd.param_groups_lrd(model_without_ddp, args.weight_decay, no_weight_decay_list=model_without_ddp.no_weight_decay(), layer_decay=args.layer_decay)
+    optimizer = torch.optim.AdamW(param_groups, lr=args.lr)
+    loss_scaler = NativeScaler(loss_scale=args.loss_scale)
+
+    misc.load_model(args=args, model_without_ddp=model_without_ddp, optimizer=optimizer, loss_scaler=loss_scaler)
+
+    if args.loss_fn == "contrastive":
+        criterion = ContrastiveLoss()
+    elif args.loss_fn == "infonce":
+        criterion = InfoNCE()
+    elif args.loss_fn == "clip_loss":
+        criterion = CLIPLoss(args)
+
+    print("criterion = %s" % str(criterion))
+
+    
+    if args.eval:
+        test_stats = evaluate(args=args, data_loader=data_loader_val, model=fuse_model, criterion=criterion, device=device)
+        print(f"Accuracy of the network on the {len(dataset_val)} test images for superkingdom: {test_stats['acc1_0']:.1f}%")
+        print(f"Accuracy of the network on the {len(dataset_val)} test images for phylum: {test_stats['acc1_1']:.1f}%")
+        print(f"Accuracy of the network on the {len(dataset_val)} test images for genus: {test_stats['acc1_2']:.1f}%")
+        exit(0)
+
+    print(f"Start training for {args.epochs} epochs")
+    start_time = time.time()
+    for epoch in range(args.start_epoch, args.epochs):
+        if args.distributed:
+            data_loader_train.sampler.set_epoch(epoch)
+
+        train_stats = train_one_epoch(fuse_model, criterion, data_loader_train, optimizer, device, epoch, loss_scaler, args.clip_grad, log_writer=log_writer, args=args)
+
+        if args.output_dir:
+
+            misc.save_model(args=args, model=fuse_model, model_without_ddp=model_without_ddp, optimizer=optimizer, loss_scaler=loss_scaler, epoch=epoch)
+
+        test_stats = evaluate(args=args, data_loader=data_loader_val, model=fuse_model, criterion=criterion, device=device)
+
+        if log_writer is not None:
+            for k in test_stats:
+                log_writer.add_scalar(f'perf/test_{k}', test_stats[k], epoch)
+
+        log_stats = {**{f'train_{k}': v for k, v in train_stats.items()}, **{f'test_{k}': v for k, v in test_stats.items()}, 'epoch': epoch, 'n_parameters': n_parameters}
+
+        if args.output_dir and misc.is_main_process():
+            if log_writer is not None:
+                log_writer.flush()
+            with open(os.path.join(args.output_dir, "log.txt"), mode="a", encoding="utf-8") as f:
+                f.write(json.dumps(log_stats) + "\n")
+
+    total_time = time.time() - start_time
+    total_time_str = str(datetime.timedelta(seconds=int(total_time)))
+    print('Training time {}'.format(total_time_str))
+
+
+if __name__ == '__main__':
+    args = get_args_parser()
+    args = args.parse_args()
+    assert (args.amp and args.loss_scale) or (not args.amp and not args.loss_scale), "Mixed precision training requires loss scaling"
+    if args.output_dir:
+        Path(args.output_dir).mkdir(parents=True, exist_ok=True)
+    main(args)
