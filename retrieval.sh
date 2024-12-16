e9ef6629eb64112714be98c12395771c8cb44f8a
jiaruizhou <jiaruizhou@mail.ustc.edu.cn>
2024-12-16 11:23:05 +0800

    changes

--- retrieval.sh
+++ retrieval.sh
@@ -15,10 +15,10 @@ PRETRAIN_MODEL=train_vit_freeze_bert_lr1e-4
 MODEL=$PRETRAIN_MODEL
 for DATA in "non_similar" "similar" "final" ; do
     mkdir -p ./contrastive_output/$MODEL/$DATA 
-    # python -m torch.distributed.launch --nproc_per_node 4 retrieval.py \
-    #     --get_encode --cross_gpu_sample --model $MODEL --name vit --contrastive_resume ./contrastive_output/$PRETRAIN_MODEL/$DATA/checkpoint-2.pth \
-    #     --batch_size 1000 --global_pooling_vit_bert --global_pool_vit --vit2bert_proj \
-    #     --data $DATA  2>&1 | tee ./contrastive_output/$MODEL/$DATA/retrieval_get_encode.log
+    python -m torch.distributed.launch --nproc_per_node 4 retrieval.py \
+        --get_encode --cross_gpu_sample --model $MODEL --name vit --contrastive_resume ./contrastive_output/$PRETRAIN_MODEL/$DATA/checkpoint-2.pth \
+        --batch_size 1000 --global_pooling_vit_bert --global_pool_vit --vit2bert_proj \
+        --data $DATA  2>&1 | tee ./contrastive_output/$MODEL/$DATA/retrieval_get_encode.log
 
     python retrieval.py --single_gpu \
         --model $MODEL --name vit --contrastive_resume ./contrastive_output/$PRETRAIN_MODEL/$DATA/checkpoint-2.pth \
